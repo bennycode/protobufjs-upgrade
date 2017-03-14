9581a932b9231a008180005085cdff2a6a6a63bf
Benny Neugebauer <bn@bennyn.de>
2017-03-14 11:08:40 +0100

    Trying to update...

--- spec/ProtoSpec.js
+++ spec/ProtoSpec.js
@@ -1,32 +1,30 @@
 var protobuf = require('protobufjs');
 
-describe('protobuf.js v5.0.1', function() {
+describe('protobuf.js v6.6.5', function() {
 
   var buffers = undefined;
 
   beforeAll(function(done) {
     var file = 'node_modules/wire-webapp-protocol-messaging/proto/messages.proto';
 
-    protobuf.loadProtoFile(file, function(error, builder) {
-      if (error) {
-        done.fail(error);
-      } else {
-        buffers = builder.build();
+    protobuf.load(file)
+      .then(function(root) {
+        buffers = root;
         done();
-      }
-    });
+      })
+      .catch(done.fail);
   });
 
   it('creates a generic message with text content', function() {
-    var text = new buffers.Text('Hello');
-    var genericMessage = new buffers.GenericMessage('id');
-    genericMessage.set('text', text);
+    var text = buffers.nested.Text.create({content: 'Hello'});
+    var genericMessage = buffers.nested.GenericMessage.create({message_id: 'id', text: text});
 
     expect(genericMessage.content).toBe('text');
     expect(genericMessage.message_id).toBe('id');
     expect(genericMessage.text.content).toBe('Hello');
 
-    var buffer = genericMessage.toArrayBuffer();
+    var GenericMessage = buffers.lookup("GenericMessage");
+    var buffer = GenericMessage.encode(genericMessage).finish();
     var typedArray = new Uint8Array(buffer);
     var expectedArray = new Uint8Array([10, 2, 105, 100, 18, 7, 10, 5, 72, 101, 108, 108, 111]);
     expect(typedArray).toEqual(expectedArray);
